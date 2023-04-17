ddf569024212a4d9121ceab41c1c16ec3c2140ff
weitang <lzc1617479219@gmail.com>
2023-04-17 10:09:41 +0800

    feat: 一些重复的添加页面步骤

---
+++ miniprogram/pages/collections/collections.ts
@@ -0,0 +1,83 @@
+// pages/collections/collections.ts
+Page({
+  data: {
+    tabList: [
+      '寻主',
+      '寻物'
+    ],
+    lostList: [
+      {
+        image: '../../assets/images/swipe1.jpg',
+        name: '保温杯',
+        region: '成都校区',
+        date: '2023年3月20日',
+        desc: 'dnawdafkjfnjaknd',
+        time: '2023年3月23日'
+      },
+      {
+        image: '../../assets/images/swipe1.jpg',
+        name: 'res杯',
+        region: '南充校区',
+        date: '2023年3月10日',
+        desc: 'dnawdafkjfn大大的娃大jaknd',
+        time: '2023年3月33日'
+      }
+    ]
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad() {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady() {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow() {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide() {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload() {
+
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh() {
+
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom() {
+
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage() {
+
+  }
+})
---
+++ miniprogram/pages/myPublishLost/myPublishLost.ts
@@ -0,0 +1,29 @@
+Page({
+  data: {
+    tabList: [
+      '寻主',
+      '寻物'
+    ],
+    lostList: [
+      {
+        image: '../../assets/images/swipe1.jpg',
+        name: '保温杯',
+        region: '成都校区',
+        date: '2023年3月20日',
+        desc: 'dnawdafkjfnjaknd',
+        time: '2023年3月23日'
+      },
+      {
+        image: '../../assets/images/swipe1.jpg',
+        name: 'res杯',
+        region: '南充校区',
+        date: '2023年3月10日',
+        desc: 'dnawdafkjfn大大的娃大jaknd',
+        time: '2023年3月33日'
+      }
+    ]
+  },
+  onLoad() {
+
+  }
+})
