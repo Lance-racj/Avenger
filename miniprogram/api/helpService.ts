8841559a07e64d01c45b2a860f8eaa4f52c82ea6
weitang <lzc1617479219@gmail.com>
2023-05-01 16:57:58 +0800

    fix: 注释添加

--- miniprogram/api/helpService.ts
+++ miniprogram/api/helpService.ts
@@ -17,20 +17,31 @@ class HelpService {
   async getHelpList(params?: {openid: string}): Promise<helpItem[]> {
     return httpRequest.get<helpItem[]>(`${baseUrl}/help/list`, params);
   }
-
-
+  /**
+   * get 获取求助收藏列表
+   * @param params ....
+   */
   async getIdleFollowList(params: {openid: string}): Promise<helpItemDetail[]> {
     return httpRequest.get<helpItemDetail[]>(`${baseUrl}/idle/follow/list`, params);
   }
-
+  /**
+   * post 收藏求助贴
+   * @param params ....
+   */
   async idleFollow(params: helpItem): Promise<string> {
     return httpRequest.post<string>(`${baseUrl}/idle/follow/add`, params);
   }
-
+  /**
+   * post 取消求助收藏
+   * @param params ....
+   */
   async idleUnFollow(params: {id: string, openid: string}): Promise<string> {
     return httpRequest.post<string>(`${baseUrl}/idle/follow/del`, params);
   }
-
+  /**
+   * check 检查单个求助收藏状态
+   * @param params ....
+   */
   async checkIdleItemFollow(params: {id: string, openid: string}): Promise<string> {
     return httpRequest.get<string>(`${baseUrl}/idle/follow/item`, params);
   }
--- miniprogram/api/idleService.ts
+++ miniprogram/api/idleService.ts
@@ -32,51 +32,76 @@ class IdleService {
   async getNeedList(params?: { openid: string }): Promise<needItem[]> {
     return httpRequest.get<needItem[]>(`${baseUrl}/idle/need/list`, params);
   }
-
+  /**
+   * delete 删除闲置发布
+   * @param params ...
+   */
   async deleteIdleItem(params: { _id: string }): Promise<string> {
     return httpRequest.post<string>(`${baseUrl}/idle/del`, params);
   }
-
+  /**
+   * delete 删除求购发布
+   * @param params ...
+   */
   async deleteNeedItem(params: { _id: string }): Promise<string> {
     return httpRequest.post<string>(`${baseUrl}/idle/need/del`, params);
   }
-
-
+  /**
+   * get 获取闲置收藏列表
+   * @param params ...
+   */
   async getIdleFollowList(params: {openid: string}): Promise<idleItemDetail[]> {
     return httpRequest.get<idleItemDetail[]>(`${baseUrl}/idle/follow/list`, params);
   }
-
+  /**
+   * get 获取求购收藏列表
+   * @param params ...
+   */
   async getNeedFollowList(params: {openid: string}): Promise<needItemDetail[]> {
     return httpRequest.get<needItemDetail[]>(`${baseUrl}/idle/need/follow/list`, params);
   }
-
+  /**
+   * post 收藏闲置发布帖
+   * @param params ...
+   */
   async idleFollow(params: idleItem): Promise<string> {
     return httpRequest.post<string>(`${baseUrl}/idle/follow/add`, params);
   }
-
+  /**
+   * post 收藏求购闲置帖
+   * @param params ...
+   */
   async needFollow(params: needItem) {
     return httpRequest.post<string>(`${baseUrl}/idle/need/follow/add`, params);
   }
-
+  /**
+   * post 取消收藏闲置发布帖
+   * @param params ...
+   */
   async idleUnFollow(params: {id: string, openid: string}): Promise<string> {
     return httpRequest.post<string>(`${baseUrl}/idle/follow/del`, params);
   }
-
+  /**
+   * post 取消收藏求购闲置帖
+   * @param params ...
+   */
   async needUnFollow(params: {id: string, openid: string}): Promise<string> {
     return httpRequest.post<string>(`${baseUrl}/idle/need/follow/del`, params);
   }
-
+  /**
+   * check 检查单个闲置收藏状态
+   * @param params ...
+   */
   async checkIdleItemFollow(params: {id: string, openid: string}): Promise<string> {
     return httpRequest.get<string>(`${baseUrl}/idle/follow/item`, params);
   }
-
+  /**
+   * check 检查单个求购收藏状态
+   * @param params ...
+   */
   async checkNeedItemFollow(params: {id: string, openid: string}): Promise<string> {
     return httpRequest.get<string>(`${baseUrl}/idle/need/follow/item`, params);
   }
-
-
-
-
 }
 
 export default new IdleService();
